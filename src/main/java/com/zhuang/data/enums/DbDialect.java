707e6b693207952efdff91511a1fef1ac0fcd255
weibin268 <binweizhuang@hotmail.com>
2019-09-28 22:03:30 +0800

    aa

--- src/main/java/com/zhuang/data/enums/DbDialect.java
+++ src/main/java/com/zhuang/data/enums/DbDialect.java
@@ -1,9 +1,25 @@
 package com.zhuang.data.enums;
 
+import java.util.Arrays;
+
 public enum DbDialect {
-	MySQL,
-	MSSQL,
-	Oracle,
-	DB2,
-	Sybase,
+	MySQL("MySQL"),
+	MSSQL("SQL Server"),
+	Oracle("Oracle"),
+	DB2("DB2"),
+	Sybase("Adaptive Server Enterprise");
+
+	private String productName;
+
+	DbDialect(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductName(){
+		return productName;
+	}
+
+	public static DbDialect getByProductName(String productName){
+		return Arrays.stream(DbDialect.values()).filter(c->c.getProductName().equals(productName)).findFirst().get();
+	}
 }
--- src/main/java/com/zhuang/data/util/DbDialectUtils.java
+++ src/main/java/com/zhuang/data/util/DbDialectUtils.java
@@ -5,64 +5,24 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import com.zhuang.data.enums.DbDialect;
-import com.zhuang.data.enums.DbProductNames;
-import com.zhuang.data.mybatis.enums.DatabaseIdValues;
 
 public class DbDialectUtils {
 
-	public static DbDialect getDbDialectByMyBatisDatabaseId(String databaseId) {
-		if (databaseId.equals(DatabaseIdValues.mysql)) {
-			return DbDialect.MySQL;
+    public static DbDialect getDbDialectByDataSource(DataSource dataSource) {
+        try {
+            return getDbDialectByConnection(dataSource.getConnection());
+        } catch (SQLException e) {
+            throw new RuntimeException("DbDialectUtil.getDbDialectByDataSource获取失败", e);
+        }
+    }
 
-		} else if (databaseId.equals(DatabaseIdValues.oracle)) {
-			return DbDialect.Oracle;
-
-		} else if (databaseId.equals(DatabaseIdValues.mssql)) {
-			return DbDialect.MSSQL;
-
-		} else if (databaseId.equals(DatabaseIdValues.db2)) {
-			return DbDialect.DB2;
-
-		} else if (databaseId.equals(DatabaseIdValues.sybase)) {
-			return DbDialect.Sybase;
-
-		} else {
-			throw new RuntimeException("databaseId:" + databaseId + ",没有找到对应的DbDialect");
-		}
-	}
-
-	public static DbDialect getDbDialectByDbProductName(String dbProductName) {
-		if (dbProductName.equals(DbProductNames.MySQL.getName())) {
-			return DbDialect.MySQL;
-		} else if (dbProductName.equals(DbProductNames.Oracle.getName())) {
-			return DbDialect.Oracle;
-		} else if (dbProductName.equals(DbProductNames.MSSQL.getName())) {
-			return DbDialect.MSSQL;
-		} else if (dbProductName.equals(DbProductNames.DB2.getName())) {
-			return DbDialect.DB2;
-		} else if (dbProductName.equals(DbProductNames.Sybase.getName())) {
-			return DbDialect.Sybase;
-		} else {
-			throw new RuntimeException("dbProductName:" + dbProductName + ",没有找到对应的DbDialect");
-		}
-	}
-
-	public static DbDialect getDbDialectByDataSource(DataSource dataSource) {
-		try {
-			return getDbDialectByConnection(dataSource.getConnection());
-		} catch (SQLException e) {
-			throw new RuntimeException("DbDialectUtil.getDbDialectByDataSource获取失败", e);
-		}
-	}
-
-	public static DbDialect getDbDialectByConnection(Connection connection) {
-		String dbProductName;
-		try {
-			dbProductName = connection.getMetaData().getDatabaseProductName();
-			return getDbDialectByDbProductName(dbProductName);
-
-		} catch (SQLException e) {
-			throw new RuntimeException("DbDialectUtil.getDbDialectByConnection获取失败", e);
-		}
-	}
+    public static DbDialect getDbDialectByConnection(Connection connection) {
+        String dbProductName;
+        try {
+            dbProductName = connection.getMetaData().getDatabaseProductName();
+            return DbDialect.getByProductName(dbProductName);
+        } catch (SQLException e) {
+            throw new RuntimeException("DbDialectUtil.getDbDialectByConnection获取失败", e);
+        }
+    }
 }
